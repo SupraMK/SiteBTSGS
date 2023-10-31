453223ddf7f56695ff3a7770c683d3f3bd8f41c7
INGuillaume <144143175+SupraMK@users.noreply.github.com>
2023-10-31 16:25:19 +0100

    Add files via upload

---
+++ certif/certif.js
@@ -0,0 +1,11 @@
+function test(){
+    var image = document.getElementById('img1');
+    var source = image.src;
+    window.open(source);
+}
+
+function test2(){
+    var image = document.getElementById('img2');
+    var source = image.src;
+    window.open(source);
+}
